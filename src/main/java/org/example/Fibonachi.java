071ee78ad1d7371c10ff6f4263294813adc9a727
Klimentiy <klimentijkvlvsk@gmail.com>
2024-11-26 10:54:04 +0300

    made a kollokvium

---
+++ src/main/java/org/example/Fibonachi.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fibonachi{
+
+    public static List<Long> makeFibonachiList(int n){
+        if(n > 93){
+            System.out.println("Too big number for function");
+            return null;
+        }
+        List<Long> arr = new ArrayList<>();
+        long a = 0, b = 1, c;
+        for(int i = 0; i < n; i++){
+            arr.add(a);
+            b+=a;
+            a = b-a;
+        }
+
+        return arr;
+    }
+}
